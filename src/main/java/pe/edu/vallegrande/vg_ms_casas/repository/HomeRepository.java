2cd3b86c9e00dc9f6fe89867f4a467cbd662bc20
jonas Zubieta león <89934911+jonaszubietaleon@users.noreply.github.com>
2025-05-26 16:34:17 -0500

    Delete src directory

--- src/main/java/pe/edu/vallegrande/vg_ms_casas/repository/HomeRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.vallegrande.vg_ms_casas.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import pe.edu.vallegrande.vg_ms_casas.model.Home;
-import reactor.core.publisher.Flux;
-
-public interface HomeRepository extends ReactiveCrudRepository<Home, Integer> {
-    Flux<Home> findByStatus(String status);
-}
